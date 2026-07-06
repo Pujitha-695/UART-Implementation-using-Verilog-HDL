9fe8ce30971ee31465bddcea8685663b77edc213
Pujitha-695 <pujithanallani@gmail.com>
2026-07-06 11:14:17 +0530

    Update DeFrame.v
    Updated author information and date in DeFrame.v

--- UART-Implementation-using-Verilog-HDL-main/Code/Full-Body/DeFrame.v
+++ UART-Implementation-using-Verilog-HDL-main/Code/Full-Body/DeFrame.v
@@ -1,9 +1,8 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
+//  AUTHOR:NALLANI PUJITHA.
+//  INFO.:  Undergraduate ECE student,NATIONAL INSTITUTE OF TECHNOLOGY DURGAPUR,WEST BENGAL
 //  FILE NAME: DeFrame.v
 //  TYPE: module.
-//  DATE: 31/8/2022
+//  DATE: 31/1/2026
 //  KEYWORDS: Frame, Data.
 //  PURPOSE: An RTL modelling for separating the frame
 //  into Data bits, Parity bit, Start and Stop bits.
