a381828b63a64e6b305a3f651859f453c061c667
Pujitha-695 <pujithanallani@gmail.com>
2026-07-06 11:28:56 +0530

    initial commit
    Updated author information and date in comments.

--- UART-Implementation-using-Verilog-HDL-main/Code/UART-Rx/RxUnit.v
+++ UART-Implementation-using-Verilog-HDL-main/Code/UART-Rx/RxUnit.v
@@ -1,9 +1,6 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
 //  FILE NAME: RxUnit.v
 //  TYPE: module.
-//  DATE: 31/8/2022
+//  DATE: 31/1/2026
 //  KEYWORDS: Reciever, UART-Rx.
 //  PURPOSE: An RTL modelling for the Top module for the Reciever unit.
 //  Architecture designed by Mohamed Maged,
