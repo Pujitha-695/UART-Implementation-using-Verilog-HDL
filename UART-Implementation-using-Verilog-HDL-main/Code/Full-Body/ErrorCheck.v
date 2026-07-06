d478ea0135ad355ceafa3e1c88f161fa64a5a13f
Pujitha-695 <pujithanallani@gmail.com>
2026-07-06 11:18:37 +0530

    .
    Updated the date in the file header comment.

--- UART-Implementation-using-Verilog-HDL-main/Code/Full-Body/ErrorCheck.v
+++ UART-Implementation-using-Verilog-HDL-main/Code/Full-Body/ErrorCheck.v
@@ -1,9 +1,6 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
 //  FILE NAME: ErrorCheck.v
 //  TYPE: module.
-//  DATE: 31/8/2022
+//  DATE: 31/1/2026
 //  KEYWORDS: Parity, Error.
 //  PURPOSE: An RTL modelling for checking if the data are transmitted correctly,
 //  checks the whole package including the start and stop bits.
