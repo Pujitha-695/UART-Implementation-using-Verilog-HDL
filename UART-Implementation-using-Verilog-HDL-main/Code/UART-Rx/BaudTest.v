794d81db6371fe0954706e064a69bdad8a1f050c
Pujitha-695 <pujithanallani@gmail.com>
2026-07-06 11:25:02 +0530

    initial commit
    Updated author information and date in comments.

--- UART-Implementation-using-Verilog-HDL-main/Code/UART-Rx/BaudTest.v
+++ UART-Implementation-using-Verilog-HDL-main/Code/UART-Rx/BaudTest.v
@@ -1,9 +1,6 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
 //  FILE NAME: BaudTest.v
 //  TYPE: Test fixture "Test bench".
-//  DATE: 31/8/2022
+//  DATE: 31/1/2026
 //  KEYWORDS: Baud Rate, clock Generator.
 
 `timescale 1ns/1ps
